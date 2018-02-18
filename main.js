b3ae5f6ce5685b2e0dd0d8fb0025a5ee3c73a483
Kim <kpayne9009@gmail.com>
2018-02-17 19:01:16 -0500

    added prompts

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var $panels = $('.panel');
 var $buttons = $('.btn');
 
 $('#page1').click(function(){
-alert("You're a pretty cool kid.");
-
+     var name;
+var name = prompt("What's your name, kid?");
+alert("You're a pretty cool kid, " +name);
 });

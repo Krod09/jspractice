c66c3f1d4a059df01b1b10f3c119b3eedc208c51
Kim <kpayne9009@gmail.com>
2018-02-17 20:23:20 -0500

    added sign-up form and success page

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 var $panels = $('.panel');
+var $tab1 = $('#page1');
+var $tab2 = $('#page2');
+var $tab3 = $('#page3');
 
-var $buttons = $('.btn');
-
-$('#page1').click(function(){
-     var name;
-var name = prompt("What's your name, kid?");
+$tab1.click(function(){
+var name = "whatever your name is...";
+name = prompt("What's your name, kid?");
 alert("You're a pretty cool kid, " +name);
+});
+
+$('#submitbtn').click(function(){
+     alert("Thanks for subscribing! Click the confirmation link in your inbox.");
 });
